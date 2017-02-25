b69b1f070620cc49fb5744779a91bfdae3829831
enzo <jingshierr@126.com>
2017-02-25 20:33:21 +0800

    add typescript 20170225

---
+++ projects/typescript/demo/Classdemo.ts
@@ -0,0 +1,56 @@
+// class  Greeter
+// {
+//     greet()
+//     {
+//         console.log("say hello");
+//     }
+// }
+//
+// let greeter = new Greeter();
+// // greeter.greet();
+//
+// class Animal{
+//
+//     name :string;
+//     constructor(theName:string) {this.name = theName;}
+//     move(distanceInMeters:number = 0)
+//     {
+//         console.log(`${this.name} moved ${distanceInMeters}m.`);
+//     }
+// }
+//
+// class  Snake extends Animal
+// {
+//     constructor(name:string){
+//         super(name);
+//     }
+//
+//     move(distanceInMeters = 5)
+//     {
+//         console.log("slithering ..");
+//         super.move(distanceInMeters);
+//     }
+// }
+//
+// class  Horse extends  Animal
+// {
+//     constructor(name:string){ super(name);}
+//     move(distanceInMeters = 45)
+//     {
+//         console.log("Galloping...");
+//         super.move(distanceInMeters);
+//     }
+//
+// }
+//
+// // let sam = new Snake("sammy th python");
+// // sam.move();
+//
+// let nel  = new  Horse("")
+// nel.move(11);
+//
+// //调用最父亲的节点
+//
+//
+
+
