b8571126a1f2c72f344ba2e0c1e6d8a456254fb4
enzo <jingshierr@126.com>
2017-02-25 21:00:03 +0800

    add React Project

---
+++ projects/React/my-app/src/helloworld_react.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2017/2/23.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+<div> <h1>{1+1}</h1> </div>,
+    document.getElementById('root')
+);
